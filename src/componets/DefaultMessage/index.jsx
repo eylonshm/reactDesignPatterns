12c86b2e0955ed25299ce281cc785702d3be49b6
Eylon Shmilovich <eylonshm@gmail.com>
2023-09-28 18:59:04 +0300

    Update Default Messsage

--- src/componets/DefaultMessage/index.jsx
+++ src/componets/DefaultMessage/index.jsx
@@ -5,6 +5,17 @@ const DefaultMessage = () => {
     <>
       <Helmet title='React Design Patterns' />
       <h2>Just use the navBar above to move between patterns 👆🏼</h2>
+      <h4 style={{ marginBottom: '0' }}>
+        You won't really find knowledge here
+      </h4>
+      <p style={{ marginTop: '0' }}>
+        Browse the repo for more examples and explanations on how to use each
+        pattern. If you have any questions or suggestions, feel free to reach
+        out to me on Github or send me an email at eylonshm@gmail.com
+      </p>
+      <a href='https://github.com/eylonshm/reactDesignPatterns' target='_blank'>
+        Github Repo
+      </a>
     </>
   )
 }
